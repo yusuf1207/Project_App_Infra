eade9f26c9e65eafe9a55e5eedefe359e63d11df
yusuf1207 <mukhammadturgunov@gmail.com>
2023-03-22 23:15:02 +0000

    update

--- app_infra/ec2/main.tf
+++ app_infra/ec2/main.tf
@@ -5,7 +5,7 @@ module "ec2_module" {
   key_name                    = "mackey"
   subnet_id                   = "subnet-0ab2fb103e79db61b"
   aws_security_group_id       = ["sg-039f5dcd7d53d3ee5"]
-  iam_instance_profile = false
+  iam_instance_profile        = ""
   associate_public_ip_address = true
   tags = {
     Name = "Main"
